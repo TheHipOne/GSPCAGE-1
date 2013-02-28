02abbc7b0e1777390b70984d5dadbdef31eff541
Bryan Sweeney <bsweeney68@yahoo.com>
2013-02-28 08:22:22 -0500

    Added Physics Engine to Main folder referenced by main; Added AI files to repository - the ones I had;
    Physics engine hooked in to both the main folder and the GSPMain-master;
    AI files added to GSPCAGE Folder - includes corrected - two files I
    didn't have 1.) MovingEntity.h, 2.) Game.h one called in Projectiles.h
    the other in Projectiles.cpp; don't have so couldn't add, laft the rest
    as I found it:)

---
+++ AICore_Files/Bounds Detection/Constants.h
@@ -0,0 +1,12 @@
+/*****************************************************************
+Program Name: Constants.h
+Name: Zach Fetters
+Description: This file is used to hold various constant variables
+			 that will be used in multiple other files
+*****************************************************************/
+
+#include <math.h>
+#include <iomanip>
+
+const double MaxDouble = (std::numeric_limits<double>::max)();
+const double MinDouble = (std::numeric_limits<double>::min)();
